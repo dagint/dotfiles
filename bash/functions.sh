e6a2ece599f235ab837e26163d106dbc54e27bf4
Michael Ginter <dev.Ginter.Michael@dev.bwater.com>
2026-01-20 09:23:19 -0500

    feat: additional quality of life updates

--- bash/functions.sh
+++ bash/functions.sh
@@ -59,3 +59,8 @@ path-append() {
         PATH="$PATH:$1"
     fi
 }
+
+# Safe, read-only git branch parsing for prompt
+parse_git_branch() {
+    git branch 2>/dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
+}
